ba6e5da27eb8dee4038a0e1a56892fe1ee9b8ca5
Ren Rizzolo <ren@jewishnews.net.au>
2021-01-14 15:15:58 +1100

    just return the html

--- src/components/Blocks/CoreImageBlock/CoreImageBlock.js
+++ src/components/Blocks/CoreImageBlock/CoreImageBlock.js
@@ -22,6 +22,10 @@ const CoreImageBlock = props => {
   } = props.attributes || {}
   const { img } = props.block
 
+  if (!img)
+    return (
+      <div dangerouslySetInnerHTML={{ __html: props.block?.originalContent }} />
+    )
   let blockClass = "core-block-image margin-bottom-md"
 
   const classes = []
